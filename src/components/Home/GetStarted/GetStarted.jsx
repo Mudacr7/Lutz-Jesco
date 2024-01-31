25c7c85e765a66e13d3a2bdeaa08af35835e0a62
Mudacr7 <123542809+Mudacr7@users.noreply.github.com>
2024-01-31 19:41:35 +0500

    m
    m

--- src/components/Home/GetStarted/GetStarted.jsx
+++ src/components/Home/GetStarted/GetStarted.jsx
@@ -8,11 +8,12 @@ const GetStarted = () => {
                     <div className="content d-flex justify-content-between align-items-center">
                         <span className="text-light">
                             <h2>Get Started</h2>
-                            <p className='fs-6'>Ready to take the first step towards your custom pumping solution? Click below to get started.</p>
+                            <p className='fs-6 mt-2'>Ready to take the first step towards your custom pumping solution? Click below to get started.</p>
                         </span>
-                        <a href="/" className='web-btn'>GetStarted</a>
+                        <a href="/" className='web-btn'>Get Started</a>
                     </div>
                 </div>
+                
             </section>
         </>
     )
